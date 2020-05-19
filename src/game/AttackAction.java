25d22de263cf077c98418ccc0d4dbd2e16f46fac
ylai0018 <ylai0018@student.monash.edu>
2020-05-19 21:52:31 +0800

    added probability to for bite attack

--- src/game/AttackAction.java
+++ src/game/AttackAction.java
@@ -32,20 +32,36 @@ public class AttackAction extends Action {
 		this.target = target;
 	}
 
+	public static boolean getBooleanWithProbability(int probabilityOfTrue) {	// Gets a random boolean with a probability of being true.
+		if (probabilityOfTrue <= 0) {
+			return false;
+		} else {
+			return new Random().nextInt(100) + 1 <= probabilityOfTrue;
+		}
+	}
+
 	@Override
 	public String execute(Actor actor, GameMap map) {
 
-		Weapon weapon = actor.getWeapon();
+		Weapon weapon = actor.getWeapon();		// get weapon. Return WeaponItem, else Intrinsic Weapon
 
-		if (rand.nextBoolean()) {
-			return actor + " misses " + target + ".";
+		if (actor.getClass().getName().equals("game.Zombie") && weapon.verb().equals("bites")){	// Zombie's bite action
+				if (!getBooleanWithProbability(30)){	// only 30% change of biting. if false, means do not bite
+					return actor + " misses " + target + ".";    //	misses the target
+				}
+		}
+		else {		// if not Zombie or the Zombie's IntrinsicWeapon is 'punches'
+			if (rand.nextBoolean()) {            // Generate a random generator. 50% probability
+				return actor + " misses " + target + ".";    //	misses the target
+			}
 		}
 
-		int damage = weapon.damage();
+
+		int damage = weapon.damage();	// Has either WeaponItem or Intrinsic Item
 		String result = actor + " " + weapon.verb() + " " + target + " for " + damage + " damage.";
 
-		target.hurt(damage);
-		if (!target.isConscious()) {
+		target.hurt(damage);	// minus hitpoints of target due to damage
+		if (!target.isConscious()) {		// target is dead
 			Item corpse = new PortableItem("dead " + target, '%');
 			map.locationOf(target).addItem(corpse);
 			
