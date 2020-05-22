b21747ae55ae258d86b290573ec050e1341cdc0e
ylai0018 <ylai0018@student.monash.edu>
2020-05-22 23:49:57 +0800

    Minor changes

--- src/game/Corpse.java
+++ src/game/Corpse.java
@@ -4,6 +4,7 @@ import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.GameMap;
 import edu.monash.fit2099.engine.Location;
 
+
 /**
  * Class representing Corpse that is able to rise as a Zombie
  */
