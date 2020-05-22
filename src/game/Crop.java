fce241cfd3fec3c738fd24c0972e893e0ea9cdd1
Tan Sook Mun <stan0111@student.monash.edu>
2020-05-22 23:48:57 +0800

    adding class diagram

--- src/game/Crop.java
+++ src/game/Crop.java
@@ -2,7 +2,6 @@ package game;
 
 import edu.monash.fit2099.engine.Actor;
 import edu.monash.fit2099.engine.Ground;
-import edu.monash.fit2099.engine.Item;
 import edu.monash.fit2099.engine.Location;
 
 /**
