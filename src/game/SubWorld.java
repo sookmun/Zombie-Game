297e3acf37476341a5edc4e70990d0cd7c44e741
ylai0018 <ylai0018@student.monash.edu>
2020-06-12 21:07:39 +0800

    updated recommendations

--- src/game/SubWorld.java
+++ src/game/SubWorld.java
@@ -61,7 +61,7 @@ public class SubWorld extends World {
                                 }
                             }
                         }
-                }
+                    }
                 }
             }
 
