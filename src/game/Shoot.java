989ce59b03ba02f7505f447648f3d6bf5f526246
Tan Sook Mun <stan0111@student.monash.edu>
2020-06-14 20:32:39 +0800

    ?

--- src/game/Shoot.java
+++ src/game/Shoot.java
@@ -61,7 +61,7 @@ public class Shoot extends Action{
                 ((Player) actor).setWeaponChosen((Weapon)weaponItem);
                 if(map.getYRange().max()>map.getXRange().max()){
                     range(actor, map, map.getYRange().max());
-                    result += SniperRifle(actor,map); 
+                    result += SniperRifle(actor,map);
                 }
                 else{
                     range(actor, map, map.getXRange().max());
